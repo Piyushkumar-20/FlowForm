640ea8ac51e6eb75635758c0e42ac1bd329ea306
Piyushkumar-20 <pk6686742@gmail.com>
2026-05-22 12:47:59 +0530

    chore: initialize pnpm workspace and add configuration files
    - Added pnpm-workspace.yaml to define workspace structure for apps and packages.
    - Created prettier.config.js for consistent code formatting.
    - Added setup.sh script to manage .env files and create symlinks in apps and packages.
    - Introduced turbo.json for task management and build configuration.

---
+++ packages/trpc/server/index.ts
@@ -0,0 +1,19 @@
+import { publicProcedure, router } from "./trpc";
+import {z} from "zod";
+import { healthRouter } from "./routes/health/route";
+import { authRouter } from "./routes/auth/route";
+
+export const serverRouter = router({
+  health: healthRouter,
+  chai: publicProcedure
+   .input(z.object({ email: z.string().email()}))
+   .output(z.object({message: z.string()}))
+  .query(( async ({input}) => {
+   return {
+    message: `Hello ${input.email}`
+   }
+  }))
+});
+
+export { createContext } from "./context";
+export type ServerRouter = typeof serverRouter;
